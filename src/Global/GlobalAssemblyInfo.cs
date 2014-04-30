7634576f53d48ad5897ffa2ca8640331681b33f7
MaxKits <macros5@ya.ru>
2014-04-30 23:15:57 +0400

    global assembly info added

---
+++ src/Global/GlobalAssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[ assembly : ComVisible( false ) ]
+[ assembly : AssemblyProduct( "MagentoAccess" ) ]
+[ assembly : AssemblyCompany( "Agile Harbor, LLC" ) ]
+[ assembly : AssemblyCopyright( "Copyright (C) 2014 Agile Harbor, LLC" ) ]
+[ assembly : AssemblyDescription( "Magento webservices API wrapper." ) ]
+[ assembly : AssemblyTrademark( "" ) ]
+[ assembly : AssemblyCulture( "" ) ]
+[ assembly : CLSCompliant( false ) ]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+// Keep in track with CA API version
+
+[ assembly : AssemblyVersion( "1.0.0.0" ) ]
