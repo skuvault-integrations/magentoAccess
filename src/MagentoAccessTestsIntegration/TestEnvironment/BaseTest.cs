9553e4bb9c25a1a8820069b9a40eaadcb8b91839
MaxKits <macros5@ya.ru>
2014-05-22 10:17:06 +0400

    code cleanup

--- src/MagentoAccessTestsIntegration/TestEnvironment/BaseTest.cs
+++ src/MagentoAccessTestsIntegration/TestEnvironment/BaseTest.cs
@@ -20,22 +20,22 @@ namespace MagentoAccessTestsIntegration.TestEnvironment
 			this._authorityUrls = this._testData.GetMagentoUrls();
 			this._accessToken = this._testData.GetMagentoAccessToken();
 
-			this._service = (this._accessToken == null || string.IsNullOrWhiteSpace(this._accessToken.AccessToken) ||string.IsNullOrWhiteSpace(this._accessToken.AccessTokenSecret)) ?
-				new MagentoService(new MagentoNonAuthenticatedUserCredentials(
+			this._service = ( this._accessToken == null || string.IsNullOrWhiteSpace( this._accessToken.AccessToken ) || string.IsNullOrWhiteSpace( this._accessToken.AccessTokenSecret ) ) ?
+				new MagentoService( new MagentoNonAuthenticatedUserCredentials(
 					this._consumer.Key,
 					this._consumer.Secret,
 					this._authorityUrls.MagentoBaseUrl,
 					this._authorityUrls.RequestTokenUrl,
 					this._authorityUrls.AuthorizeUrl,
 					this._authorityUrls.AccessTokenUrl
-					)) :
-				new MagentoService(new MagentoAuthenticatedUserCredentials(
+					) ) :
+				new MagentoService( new MagentoAuthenticatedUserCredentials(
 					this._accessToken.AccessToken,
 					this._accessToken.AccessTokenSecret,
 					this._authorityUrls.MagentoBaseUrl,
 					this._consumer.Secret,
 					this._consumer.Key
-					));
+					) );
 			this._service.AfterGettingToken += this._testData.CreateAccessTokenFile;
 		}
 	}
