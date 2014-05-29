d6165eb5c46ce6063358e804a2a63dba495c049b
MaxKits <macros5@ya.ru>
2014-05-29 22:25:53 +0400

    added method to determine order shipping status

--- src/MagentoAccess/Models/GetOrders/Order.cs
+++ src/MagentoAccess/Models/GetOrders/Order.cs
@@ -1,10 +1,21 @@
-﻿namespace MagentoAccess.Models.GetOrders
+﻿using System.Linq;
+
+namespace MagentoAccess.Models.GetOrders
 {
 	public class Order : Services.GetOrders.Order
 	{
 		public Order( Services.GetOrders.Order order )
 			: base( order )
 		{
+		}
+	}
+
+
+	public static class OrderExtensions
+	{
+		public static bool IsShipped(this Order order)
+		{
+			return order.Items.ToList().TrueForAll(x => (x.QtyOrdered - x.QtyCanceled) == (x.QtyShipped));
 		}
 	}
 }
