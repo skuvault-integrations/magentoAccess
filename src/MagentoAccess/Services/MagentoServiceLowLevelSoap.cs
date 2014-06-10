8d409fa02cc635cc31cf25f31b2e5f6fd7a9c9a2
MaxKits <macros5@ya.ru>
2014-06-10 13:23:32 +0400

    added parameters to magento service low level soap

--- src/MagentoAccess/Services/MagentoServiceLowLevelSoap.cs
+++ src/MagentoAccess/Services/MagentoServiceLowLevelSoap.cs
@@ -9,35 +9,26 @@ namespace MagentoAccess.Services
 	{
 		private readonly Mage_Api_Model_Server_Wsi_HandlerPortTypeClient _magentoSoapService;
 
-		public MagentoServiceLowLevelSoap()
+		protected string UserName { get; set; }
+
+		protected string Password { get; set; }
+
+		public MagentoServiceLowLevelSoap( string userName, string password, string endpointAddress )
 		{
-			this._magentoSoapService = new Mage_Api_Model_Server_Wsi_HandlerPortTypeClient(new BasicHttpBinding(), new EndpointAddress("http://192.168.0.103/magento/index.php/api/v2_soap/index/"));
+			this.UserName = userName;
+			this.Password = password;
+			this._magentoSoapService = new Mage_Api_Model_Server_Wsi_HandlerPortTypeClient( new BasicHttpBinding(), new EndpointAddress( endpointAddress ) );
 		}
 
-		public async Task<object> GetOrders()
+		public async Task< object > GetOrders( DateTime modifiedFrom, DateTime modifiedTo )
 		{
-			var session = await _magentoSoapService.loginAsync("MaxKits", "123456").ConfigureAwait(false);
+			var session = await this._magentoSoapService.loginAsync( this.UserName, this.Password ).ConfigureAwait( false );
 
-			var res = await _magentoSoapService.salesOrderListAsync(session.result, new filters()).ConfigureAwait(false);
+			var filters = new filters { complex_filter = new complexFilter[ 2 ] };
+			filters.complex_filter[ 0 ] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "from", value = modifiedFrom.ToString() } };
+			filters.complex_filter[ 1 ] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "to", value = modifiedTo.ToString() } };
 
-			return res;
-		}
-
-		public async Task<object> GetOrders(DateTime modifiedTime)
-		{
-			var session = await _magentoSoapService.loginAsync("MaxKits", "123456").ConfigureAwait(false);
-
-			var filters = new filters();
-			//filters.filter = new associativeEntity[1];
-			//filters.filter[0] = new associativeEntity() { key = "created_at", value = "2014-05-01 10:10:00" };
-
-
-			filters.complex_filter = new complexFilter[2];
-			filters.complex_filter[0] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "from", value = "2014-05-08 15:02:58" } };
-			filters.complex_filter[1] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "to", value = "2014-05-28 10:48:52" } };
-
-
-			var res = await _magentoSoapService.salesOrderListAsync(session.result, filters).ConfigureAwait(false);
+			var res = await this._magentoSoapService.salesOrderListAsync( session.result, filters ).ConfigureAwait( false );
 
 			return res;
 		}
--- src/MagentoAccessTestsIntegration/Services/MeagentoServiceLowLevelSoapTest.cs
+++ src/MagentoAccessTestsIntegration/Services/MeagentoServiceLowLevelSoapTest.cs
@@ -21,7 +21,7 @@ namespace MagentoAccessTestsIntegration.Services
 		{
 			//this._testData = new TestData(@"..\..\Files\magento_ConsumerKey.csv", @"..\..\Files\magento_AuthorizeEndPoints.csv", @"..\..\Files\magento_AccessToken.csv", @"..\..\Files\magento_VerifierCode.csv");
 			//this._authorityUrls = this._testData.GetMagentoUrls();
-			this._service = new MagentoServiceLowLevelSoap();
+			this._service = new MagentoServiceLowLevelSoap("MaxKits", "123456", "http://192.168.0.103/magento/index.php/api/v2_soap/index/");
 		}
 
 		[ Test ]
@@ -30,7 +30,9 @@ namespace MagentoAccessTestsIntegration.Services
 			//------------ Arrange
 
 			//------------ Act
-			var getOrdersTask = this._service.GetOrders(DateTime.Now);
+			var modifiedFrom = DateTime.Parse("2014-05-08 15:02:58");
+			var modifiedTo = DateTime.Parse("2014-05-28 10:48:52");
+			var getOrdersTask = this._service.GetOrders(modifiedFrom, modifiedTo);
 
 			//------------ Assert
 			getOrdersTask.Result.Should().NotBeNull();
