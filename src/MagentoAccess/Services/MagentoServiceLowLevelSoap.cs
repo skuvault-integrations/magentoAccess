26d0002f55590b19002a5b6cca4f7591761969e2
MaxKits <macros5@ya.ru>
2014-06-10 13:43:43 +0400

    added get products to service soap, session id lifetime

--- src/MagentoAccess/Services/MagentoServiceLowLevelSoap.cs
+++ src/MagentoAccess/Services/MagentoServiceLowLevelSoap.cs
@@ -7,28 +7,60 @@ namespace MagentoAccess.Services
 {
 	internal class MagentoServiceLowLevelSoap
 	{
-		private readonly Mage_Api_Model_Server_Wsi_HandlerPortTypeClient _magentoSoapService;
+		protected readonly Mage_Api_Model_Server_Wsi_HandlerPortTypeClient _magentoSoapService;
 
 		protected string UserName { get; set; }
 
 		protected string Password { get; set; }
 
-		public MagentoServiceLowLevelSoap( string userName, string password, string endpointAddress )
+		protected string Store { get; set; }
+
+		protected string _sessionId;
+
+		protected DateTime _sessionIdCreatedAt;
+
+		protected const int SessionIdLifeTime = 3590;
+
+		protected async Task< string > GetSessionId()
+		{
+			if( string.IsNullOrWhiteSpace( this._sessionId ) || DateTime.UtcNow.Subtract( this._sessionIdCreatedAt ).TotalSeconds < SessionIdLifeTime )
+				return this._sessionId;
+
+			var getSessionIdTask = await this._magentoSoapService.loginAsync( this.UserName, this.Password ).ConfigureAwait( false );
+			this._sessionIdCreatedAt = DateTime.UtcNow;
+			return this._sessionId = getSessionIdTask.result;
+		}
+
+		public MagentoServiceLowLevelSoap( string userName, string password, string endpointAddress, string store )
 		{
 			this.UserName = userName;
 			this.Password = password;
+			this.Store = store;
 			this._magentoSoapService = new Mage_Api_Model_Server_Wsi_HandlerPortTypeClient( new BasicHttpBinding(), new EndpointAddress( endpointAddress ) );
 		}
 
 		public async Task< object > GetOrders( DateTime modifiedFrom, DateTime modifiedTo )
 		{
-			var session = await this._magentoSoapService.loginAsync( this.UserName, this.Password ).ConfigureAwait( false );
+			//var session = await this._magentoSoapService.loginAsync( this.UserName, this.Password ).ConfigureAwait( false );
+			var sessionId = await this.GetSessionId().ConfigureAwait( false );
 
 			var filters = new filters { complex_filter = new complexFilter[ 2 ] };
 			filters.complex_filter[ 0 ] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "from", value = modifiedFrom.ToString() } };
 			filters.complex_filter[ 1 ] = new complexFilter() { key = "updated_at", value = new associativeEntity() { key = "to", value = modifiedTo.ToString() } };
 
-			var res = await this._magentoSoapService.salesOrderListAsync( session.result, filters ).ConfigureAwait( false );
+			var res = await this._magentoSoapService.salesOrderListAsync( sessionId, filters ).ConfigureAwait( false );
+
+			return res;
+		}
+
+		public async Task< object > GetProducts()
+		{
+			//var sessionId = await this._magentoSoapService.loginAsync( this.UserName, this.Password ).ConfigureAwait( false );
+			var sessionId = await this.GetSessionId().ConfigureAwait( false );
+
+			var filters = new filters { complex_filter = new complexFilter[ 0 ] };
+
+			var res = await this._magentoSoapService.catalogProductListAsync( sessionId, filters, this.Store ).ConfigureAwait( false );
 
 			return res;
 		}
--- src/MagentoAccessTestsIntegration/Services/MeagentoServiceLowLevelSoapTest.cs
+++ src/MagentoAccessTestsIntegration/Services/MeagentoServiceLowLevelSoapTest.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentAssertions;
-using MagentoAccess.Models.Services.PutStockItems;
 using MagentoAccess.Services;
 using MagentoAccessTestsIntegration.TestEnvironment;
 using NUnit.Framework;
@@ -16,12 +13,12 @@ namespace MagentoAccessTestsIntegration.Services
 		private MagentoUrls _authorityUrls;
 		private MagentoServiceLowLevelSoap _service;
 
-		[SetUp]
+		[ SetUp ]
 		public void Setup()
 		{
 			//this._testData = new TestData(@"..\..\Files\magento_ConsumerKey.csv", @"..\..\Files\magento_AuthorizeEndPoints.csv", @"..\..\Files\magento_AccessToken.csv", @"..\..\Files\magento_VerifierCode.csv");
 			//this._authorityUrls = this._testData.GetMagentoUrls();
-			this._service = new MagentoServiceLowLevelSoap("MaxKits", "123456", "http://192.168.0.103/magento/index.php/api/v2_soap/index/");
+			this._service = new MagentoServiceLowLevelSoap( "MaxKits", "123456", "http://192.168.0.103/magento/index.php/api/v2_soap/index/", "0" );
 		}
 
 		[ Test ]
@@ -30,12 +27,24 @@ namespace MagentoAccessTestsIntegration.Services
 			//------------ Arrange
 
 			//------------ Act
-			var modifiedFrom = DateTime.Parse("2014-05-08 15:02:58");
-			var modifiedTo = DateTime.Parse("2014-05-28 10:48:52");
-			var getOrdersTask = this._service.GetOrders(modifiedFrom, modifiedTo);
+			var modifiedFrom = DateTime.Parse( "2014-05-08 15:02:58" );
+			var modifiedTo = DateTime.Parse( "2014-05-28 10:48:52" );
+			var getOrdersTask = this._service.GetOrders( modifiedFrom, modifiedTo );
 
 			//------------ Assert
 			getOrdersTask.Result.Should().NotBeNull();
+		}
+
+		[ Test ]
+		public void GetProducts_StoreContainsProducts_ReceiveProducts()
+		{
+			//------------ Arrange
+
+			//------------ Act
+			var getProductsTask = this._service.GetProducts();
+
+			//------------ Assert
+			getProductsTask.Result.Should().NotBeNull();
 		}
 	}
 }
