0b91a18fd8beaface8e689cb0e398a318ee3d5a3
MaxKits <macros5@ya.ru>
2014-06-11 22:23:02 +0400

    extensions tests added

---
+++ src/MagentoAccessTests/Misc/ExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using MagentoAccess.Misc;
+using NUnit.Framework;
+
+namespace MagentoAccessTests.Misc
+{
+	[ TestFixture ]
+	public class ExtensionsTest
+	{
+		[ Test ]
+		public void Test()
+		{
+			//------------ Arrange
+			var list1 = new List< int >();
+			for( var i = 0; i < 121; i++ )
+			{
+				list1.Add( i + 10 );
+			}
+
+			//------------ Act
+			var list1c = list1.SplitToChunks( 4 ).ToList();
+
+			//------------ Assert
+			list1c.Count().Should().Be( 31 );
+		}
+	}
+}
