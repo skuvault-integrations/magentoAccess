aff22178a13d3c1dc302857180edc5d63f6c3d6b
MaxKits <macros5@ya.ru>
2014-06-12 15:15:07 +0400

    updated version to rc-6

--- .build.ps1
+++ .build.ps1
@@ -100,7 +100,7 @@ task NuGet Package, Version, {
 <package>
 	<metadata>
 		<id>MagentoAccess</id>
-		<version>$Version-rc5</version>
+		<version>$Version-rc6</version>
 		<authors>Slav Ivanyuk</authors>
 		<owners>Slav Ivanyuk</owners>
 		<projectUrl>https://github.com/slav/MagentoAccess</projectUrl>
