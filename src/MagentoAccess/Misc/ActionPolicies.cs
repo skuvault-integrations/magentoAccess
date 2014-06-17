a44cbdef6b7e0b1e771fb6bbc90e3788bc6354d4
MaxKits <macros5@ya.ru>
2014-06-18 02:24:14 +0400

    action policy chenged to log MagentoLogger

--- src/MagentoAccess/Misc/ActionPolicies.cs
+++ src/MagentoAccess/Misc/ActionPolicies.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Netco.ActionPolicyServices;
-using Netco.Logging;
 using Netco.Utils;
 
 namespace MagentoAccess.Misc
@@ -10,27 +9,27 @@ namespace MagentoAccess.Misc
 	{
 		private static readonly ActionPolicy _magentoSumbitPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
 		{
-			typeof( ActionPolicies ).Log().Trace( ex, "Retrying Magento API submit call for the {0} time", i );
+			MagentoLogger.Log().Trace( ex, "Retrying Magento API submit call for the {0} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
 		} );
 
 		private static readonly ActionPolicyAsync _magentoSumbitAsyncPolicy = ActionPolicyAsync.Handle< Exception >()
 			.RetryAsync( 10, async ( ex, i ) =>
 			{
-				typeof( ActionPolicies ).Log().Trace( ex, "Retrying Magento API submit call for the {0} time", i );
+				MagentoLogger.Log().Trace( ex, "Retrying Magento API submit call for the {0} time", i );
 				await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) ).ConfigureAwait( false );
 			} );
 
 		private static readonly ActionPolicy _magentoGetPolicy = ActionPolicy.Handle< Exception >().Retry( 10, ( ex, i ) =>
 		{
-			typeof( ActionPolicies ).Log().Trace( ex, "Retrying Magento API get call for the {0} time", i );
+			MagentoLogger.Log().Trace( ex, "Retrying Magento API get call for the {0} time", i );
 			SystemUtil.Sleep( TimeSpan.FromSeconds( 0.5 + i ) );
 		} );
 
 		private static readonly ActionPolicyAsync _magentoGetAsyncPolicy = ActionPolicyAsync.Handle< Exception >()
 			.RetryAsync( 10, async ( ex, i ) =>
 			{
-				typeof( ActionPolicies ).Log().Trace( ex, "Retrying Magento API get call for the {0} time", i );
+				MagentoLogger.Log().Trace( ex, "Retrying Magento API get call for the {0} time", i );
 				await Task.Delay( TimeSpan.FromSeconds( 0.5 + i ) ).ConfigureAwait( false );
 			} );
 
