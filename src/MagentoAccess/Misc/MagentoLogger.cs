02e6cbdcf39614e5e2531ba6480bdfaed8a00563
MaxKits <maxim.kitsenko.r31@gmail.com>
2015-11-20 05:15:16 +0300

    assembly version added

--- src/MagentoAccess/Misc/MagentoLogger.cs
+++ src/MagentoAccess/Misc/MagentoLogger.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using Netco.Logging;
 
 namespace MagentoAccess.Misc
 {
 	internal class MagentoLogger
 	{
+		private static readonly FileVersionInfo _fvi;
+
+		static MagentoLogger()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			_fvi = FileVersionInfo.GetVersionInfo( assembly.Location );
+		}
+
 		public static ILogger Log()
 		{
 			return NetcoLogger.GetLogger( "MagentoLogger" );
@@ -12,22 +22,22 @@ namespace MagentoAccess.Misc
 
 		public static void LogTraceException( Exception exception )
 		{
-			Log().Trace( exception, "[magento] An exception occured." );
+			Log().Trace( exception, "[magento] An exception occured. {0}.", _fvi );
 		}
 
 		public static void LogTraceStarted( string info )
 		{
-			Log().Trace( "[magento] Start call:{0}.", info );
+			Log().Trace( "[magento] Start call:{0}. {1}.", info, _fvi );
 		}
 
 		public static void LogTraceEnded( string info )
 		{
-			Log().Trace( "[magento] End call:{0}.", info );
+			Log().Trace( "[magento] End call:{0}. {1}.", info, _fvi );
 		}
 
 		public static void LogTrace( string info )
 		{
-			Log().Trace( "[magento] Trace info:{0}.", info );
+			Log().Trace( "[magento] Trace info:{0}. {1}", info, _fvi );
 		}
 	}
 }
