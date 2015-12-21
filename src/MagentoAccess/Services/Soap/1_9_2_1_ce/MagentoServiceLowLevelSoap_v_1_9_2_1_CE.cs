696f95ed87e849cab98dbe86ede4c37b5a54c216
MaxKits <maxim.kitsenko.r31@gmail.com>
2015-12-22 01:49:28 +0300

    refactoring

--- src/MagentoAccess/Services/Soap/1_9_2_1_ce/MagentoServiceLowLevelSoap_v_1_9_2_1_CE.cs
+++ src/MagentoAccess/Services/Soap/1_9_2_1_ce/MagentoServiceLowLevelSoap_v_1_9_2_1_CE.cs
@@ -929,34 +929,4 @@ namespace MagentoAccess.Services.Soap._1_9_2_1_ce
 		}
 		#endregion
 	}
-
-	internal class StatusChecker
-	{
-		private int invokeCount;
-		private readonly int maxCount;
-
-		public StatusChecker( int count )
-		{
-			this.invokeCount = 0;
-			this.maxCount = count;
-		}
-
-		public void CheckStatus( Object stateInfo )
-		{
-			dynamic serviceClient = null;
-
-			if( stateInfo is MagentoSoapServiceReference_v_1_14_1_EE.Mage_Api_Model_Server_Wsi_HandlerPortTypeClient )
-				serviceClient = stateInfo as MagentoSoapServiceReference_v_1_14_1_EE.Mage_Api_Model_Server_Wsi_HandlerPortTypeClient;
-			else if( stateInfo is Mage_Api_Model_Server_Wsi_HandlerPortTypeClient )
-				serviceClient = stateInfo as Mage_Api_Model_Server_Wsi_HandlerPortTypeClient;
-
-			Interlocked.Increment( ref this.invokeCount );
-			if( this.invokeCount == this.maxCount )
-			{
-				Interlocked.Exchange( ref this.invokeCount, 0 );
-				if( serviceClient != null )
-					serviceClient.Abort();
-			}
-		}
-	}
 }
