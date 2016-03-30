dd9e078c1bf7334b7b471bcdbc10dd89f97cdea6
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-03-30 15:21:05 +0300

    code clean up

--- src/MagentoAccess/Services/Soap/IMagento1XxxHelper.cs
+++ src/MagentoAccess/Services/Soap/IMagento1XxxHelper.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using MagentoAccess.Models.Services.Soap.GetProducts;
 
-namespace MagentoAccess.Services.Soap._1_9_2_1_ce
+namespace MagentoAccess.Services.Soap
 {
 	internal interface IMagento1XxxHelper
 	{
-		Task<IEnumerable< ProductDetails > > FillProductDetails(IEnumerable<ProductDetails> resultProducts);
+		Task< IEnumerable< ProductDetails > > FillProductDetails( IEnumerable< ProductDetails > resultProducts );
 	}
 }
