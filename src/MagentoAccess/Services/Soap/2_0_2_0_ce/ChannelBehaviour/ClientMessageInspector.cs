1b703f50dcbb30303f4d7aa5797dcc728dbff1ba
MaxKits <maxim.kitsenko.r31@gmail.com>
2016-04-04 18:31:34 +0300

    code cleaned

--- src/MagentoAccess/Services/Soap/2_0_2_0_ce/ChannelBehaviour/ClientMessageInspector.cs
+++ src/MagentoAccess/Services/Soap/2_0_2_0_ce/ChannelBehaviour/ClientMessageInspector.cs
@@ -1,15 +1,13 @@
-using System;
 using System.IO;
 using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Dispatcher;
-using System.Text;
 using System.Xml;
 
 namespace MagentoAccess.Services.Soap._2_0_2_0_ce.ChannelBehaviour
 {
-	internal class ClientMessageInspector : IClientMessageInspector
+	internal class ClientMessageInspector: IClientMessageInspector
 	{
 		public string AccessToken;
 
@@ -37,73 +35,18 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce.ChannelBehaviour
 			else
 				httpRequestMessage.Headers.Add( "Authorization", "Bearer " + this.AccessToken );
 
-
-			//using (XmlDictionaryReader reader = request.GetReaderAtBodyContents())
-			//{
-			//	//TODO: correct solution
-			//	string content = reader.ReadOuterXml();
-			//	//content = content.Replace("agento-2-0-2-0", "xxx123");
-			//	using (var writer2 = GenerateStreamFromString(content))
-			//	using (var writer = XmlDictionaryWriter.CreateBinaryWriter(writer2))
-			//	{
-			//		request.State
-			//		request.WriteBody(writer);
-
-			//		//my write message
-			//	}
-			//	//Other stuff here...                
-			//}
-
-			Modify3(ref request,channel, "Magento-2-0-2-0-ce", "magento-2-0-2-0-ce-2" );
+			// crutch for magento 2.0
+			this.ReplaceInMessageBody( ref request, channel, "Magento-2-0-2-0-ce", "magento-2-0-2-0-ce-2" );
 
 			return null;
 		}
 
-		private void Modify2(ref Message request, IClientChannel channel)
+		private void ReplaceInMessageBody( ref Message request, IClientChannel channel, string magentoCe, string newValue )
 		{
-
-        MessageBuffer msgbuf = request.CreateBufferedCopy(int.MaxValue);
-        Message tmpMessage = msgbuf.CreateMessage();
-        XmlDictionaryReader xdr = tmpMessage.GetReaderAtBodyContents();
-        MemoryStream ms = new MemoryStream();
-       // _compiledTransform.Transform(xdr,null,ms);
-
-        ms.Position = 0;
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.Load(ms);
-
-        MemoryStream newStream = new MemoryStream();
-        xmlDoc.Save(newStream);
-        newStream.Position = 0;
-
-        //To debug contents of the stream
-        StreamReader sr = new StreamReader(newStream);
-        var temp = sr.ReadToEnd();
-        //At this point the XSLT tranforms has resulted in the fragment we want so all good!
-
-
-        XmlReaderSettings settings = new XmlReaderSettings();
-        settings.ConformanceLevel = ConformanceLevel.Fragment;
-        newStream.Position = 0;
-        XmlReader reader = XmlReader.Create(newStream,settings);
-        reader.MoveToContent();
-
-        //Reader seems to have lost the correct fragment!!! At least returned message does not contain correct fragment.
-        Message newMessage = Message.CreateMessage(request.Version, null, reader);
-        newMessage.Properties.CopyProperties(request.Properties);
-        request = newMessage;
-
-        //return request;
-
-		}
-
-		private void Modify3(ref Message request, IClientChannel channel, string magentoCe, string newValue )
-		{
-
-			using (XmlDictionaryReader reader = request.GetReaderAtBodyContents())
+			using( XmlDictionaryReader reader = request.GetReaderAtBodyContents() )
 			{
 				string content = reader.ReadOuterXml();
-				var strBuf2 = content.Replace(magentoCe, newValue);
+				var strBuf2 = content.Replace( magentoCe, newValue );
 				using( var writer2 = this.GenerateStreamFromString( strBuf2 ) )
 				using( var writer = XmlDictionaryWriter.CreateBinaryWriter( writer2 ) )
 				{
@@ -122,40 +65,11 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce.ChannelBehaviour
 			}
 		}
 
-		private void Modify1 (ref Message request, IClientChannel channel)
-		{
-
-			var buffer = request.CreateBufferedCopy(Int32.MaxValue);
-			var toPrint = buffer.CreateMessage();
-			MemoryStream ms = new MemoryStream();
-			XmlWriterSettings ws = new XmlWriterSettings
-			{
-				Indent = true,
-				IndentChars = "  ",
-				OmitXmlDeclaration = true, 
-				Encoding = new UTF8Encoding(false)
-			};
-			XmlWriter w = XmlWriter.Create(ms, ws);
-			toPrint.WriteMessage(w);
-			w.Flush();
-			var strBuf = Encoding.UTF8.GetString(ms.ToArray());
-			var strBuf2 = strBuf.Replace("Magento-2-0-2-0-ce", "magento-2-0-2-0-ce-2");
-
-			var v = System.Xml.XmlReader.Create(GenerateStreamFromString(strBuf2));
-			Message newMessage = Message.CreateMessage(request.Version, null, v);
-			newMessage.Properties.CopyProperties(request.Properties);
-			newMessage.Headers.CopyHeaderFrom(request,0);
-
-			//var modifiedReply = buffer.CreateMessage(); // need to recreate the message here
-			request = newMessage;
-
-		}
-
-		public Stream GenerateStreamFromString(string s)
+		public Stream GenerateStreamFromString( string s )
 		{
 			MemoryStream stream = new MemoryStream();
-			StreamWriter writer = new StreamWriter(stream);
-			writer.Write(s);
+			StreamWriter writer = new StreamWriter( stream );
+			writer.Write( s );
 			writer.Flush();
 			stream.Position = 0;
 			return stream;
@@ -172,7 +86,7 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce.ChannelBehaviour
 				var contentType = prop.Headers[ "Content-Type" ];
 			}
 
-			this.Modify3( ref reply, null,  "magento-2-0-2-0-ce-2","Magento-2-0-2-0-ce" );
+			this.ReplaceInMessageBody( ref reply, null, "magento-2-0-2-0-ce-2", "Magento-2-0-2-0-ce" );
 		}
 	}
 }
