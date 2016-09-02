56bccd4843760d16f66dd351f73683c42d696494
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-09-02 14:36:17 +0300

    m2.0 refactored

--- src/MagentoAccess/Services/Soap/2_0_2_0_ce/MagentoServiceLowLevelSoap_v_2_0_2_0_ce_Misc.cs
+++ src/MagentoAccess/Services/Soap/2_0_2_0_ce/MagentoServiceLowLevelSoap_v_2_0_2_0_ce_Misc.cs
@@ -38,6 +38,7 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce
 		protected const string SoapApiUrl = "soap/default?services=";
 
 		protected salesOrderRepositoryV1PortTypeClient _magentoSoapService;
+
 		protected integrationAdminTokenServiceV1PortTypeClient _magentoSoapService2;
 
 		protected string _sessionId;
@@ -53,67 +54,6 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce
 		private void LogTraceGetResponseException( Exception exception )
 		{
 			MagentoLogger.Log().Trace( exception, "[magento] SOAP throw an exception." );
-		}
-
-		public async Task< GetSessionIdResponse > GetSessionId( bool throwException = true )
-		{
-			try
-			{
-				this.getSessionIdSemaphore.Wait();
-				if( !string.IsNullOrWhiteSpace( this._sessionId ) && DateTime.UtcNow.Subtract( this._sessionIdCreatedAt ).TotalSeconds < SessionIdLifeTime )
-					return new GetSessionIdResponse( this._sessionId, true );
-
-				var sessionId = await this.PullSessionId().ConfigureAwait( false );
-
-				this._sessionIdCreatedAt = sessionId.Item2;
-				this._sessionId = sessionId.Item1;
-				this.TokenSecret = this._sessionId;
-
-				return new GetSessionIdResponse( this._sessionId, false );
-			}
-			catch( Exception exc )
-			{
-				if( throwException )
-					throw new MagentoSoapException( string.Format( "An error occured during GetSessionId()" ), exc );
-				else
-				{
-					this.LogTraceGetResponseException( exc );
-					return null;
-				}
-			}
-			finally
-			{
-				this.getSessionIdSemaphore.Release();
-			}
-		}
-
-		public MagentoServiceLowLevelSoap_v_2_0_2_0_ce( string apiUser, string apiKey, string baseMagentoUrl, string store )
-		{
-			this.ApiUser = apiUser;
-			this.ApiKey = apiKey;
-			this.Store = store;
-			this.BaseMagentoUrl = baseMagentoUrl;
-
-			this._customBinding = CustomBinding( baseMagentoUrl, MessageVersion.Soap11 );
-			this.PullSessionId = async () =>
-			{
-				if( !string.IsNullOrWhiteSpace( apiUser ) )
-				{
-					var privateClient = this.CreateMagentoServiceAdminClient( this.BaseMagentoUrl );
-					var integrationAdminTokenServiceV1CreateAdminAccessTokenRequest = new IntegrationAdminTokenServiceV1CreateAdminAccessTokenRequest() { username = this.ApiUser, password = this.ApiKey };
-					var loginResponse = await privateClient.integrationAdminTokenServiceV1CreateAdminAccessTokenAsync( integrationAdminTokenServiceV1CreateAdminAccessTokenRequest ).ConfigureAwait( false );
-					this._sessionIdCreatedAt = DateTime.UtcNow;
-					this._sessionId = loginResponse.integrationAdminTokenServiceV1CreateAdminAccessTokenResponse.result;
-				}
-				else
-				{
-					this._sessionId = this.ApiKey;
-				}
-
-				return Tuple.Create( this._sessionId, DateTime.UtcNow );
-			};
-
-			this.getSessionIdSemaphore = new SemaphoreSlim( 1, 1 );
 		}
 
 		private integrationAdminTokenServiceV1PortTypeClient CreateMagentoServiceAdminClient( string baseMagentoUrl )
@@ -247,6 +187,59 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce
 			return customBinding;
 		}
 
+		private string CreateMethodCallInfo( string methodParameters = "", Mark mark = null, string errors = "", string methodResult = "", string additionalInfo = "", [ CallerMemberName ] string memberName = "", string notes = "" )
+		{
+			mark = mark ?? Mark.Blank();
+			var connectionInfo = this.ToJsonSoapInfo();
+			var str = string.Format(
+				"{{MethodName:{0}, ConnectionInfo:{1}, MethodParameters:{2}, Mark:\"{3}\"{4}{5}{6}{7}}}",
+				memberName,
+				connectionInfo,
+				methodParameters,
+				mark,
+				string.IsNullOrWhiteSpace( errors ) ? string.Empty : ", Errors:" + errors,
+				string.IsNullOrWhiteSpace( methodResult ) ? string.Empty : ", Result:" + methodResult,
+				string.IsNullOrWhiteSpace( notes ) ? string.Empty : ", Notes:" + notes,
+				string.IsNullOrWhiteSpace( additionalInfo ) ? string.Empty : ", AdditionalInfo: " + additionalInfo
+				);
+			return str;
+		}
+
+		#region Contract
+		public MagentoServiceLowLevelSoap_v_2_0_2_0_ce( string apiUser, string apiKey, string baseMagentoUrl, string store )
+		{
+			this.ApiUser = apiUser;
+			this.ApiKey = apiKey;
+			this.Store = store;
+			this.BaseMagentoUrl = baseMagentoUrl;
+
+			this._customBinding = CustomBinding( baseMagentoUrl, MessageVersion.Soap11 );
+			this.PullSessionId = async () =>
+			{
+				if( !string.IsNullOrWhiteSpace( apiUser ) )
+				{
+					var privateClient = this.CreateMagentoServiceAdminClient( this.BaseMagentoUrl );
+					var integrationAdminTokenServiceV1CreateAdminAccessTokenRequest = new IntegrationAdminTokenServiceV1CreateAdminAccessTokenRequest() { username = this.ApiUser, password = this.ApiKey };
+					var loginResponse = await privateClient.integrationAdminTokenServiceV1CreateAdminAccessTokenAsync( integrationAdminTokenServiceV1CreateAdminAccessTokenRequest ).ConfigureAwait( false );
+					this._sessionIdCreatedAt = DateTime.UtcNow;
+					this._sessionId = loginResponse.integrationAdminTokenServiceV1CreateAdminAccessTokenResponse.result;
+				}
+				else
+				{
+					this._sessionId = this.ApiKey;
+				}
+
+				return Tuple.Create( this._sessionId, DateTime.UtcNow );
+			};
+
+			this.getSessionIdSemaphore = new SemaphoreSlim( 1, 1 );
+		}
+
+		public string ToJsonSoapInfo()
+		{
+			return $"{{BaseMagentoUrl:{( string.IsNullOrWhiteSpace( this.BaseMagentoUrl ) ? PredefinedValues.NotAvailable : this.BaseMagentoUrl )}, ApiUser:{( string.IsNullOrWhiteSpace( this.ApiUser ) ? PredefinedValues.NotAvailable : this.ApiUser )},ApiKey:{( string.IsNullOrWhiteSpace( this.ApiKey ) ? PredefinedValues.NotAvailable : this.ApiKey )},ApiToken:{( string.IsNullOrWhiteSpace( this._sessionId ) ? PredefinedValues.NotAvailable : this._sessionId )},Store:{( string.IsNullOrWhiteSpace( this.Store ) ? PredefinedValues.NotAvailable : this.Store )}}}";
+		}
+
 		public virtual async Task< GetMagentoInfoResponse > GetMagentoInfoAsync( bool suppressException )
 		{
 			try
@@ -269,28 +262,38 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce
 			}
 		}
 
-		public string ToJsonSoapInfo()
+		public async Task< GetSessionIdResponse > GetSessionId( bool throwException = true )
 		{
-			return $"{{BaseMagentoUrl:{( string.IsNullOrWhiteSpace( this.BaseMagentoUrl ) ? PredefinedValues.NotAvailable : this.BaseMagentoUrl )}, ApiUser:{( string.IsNullOrWhiteSpace( this.ApiUser ) ? PredefinedValues.NotAvailable : this.ApiUser )},ApiKey:{( string.IsNullOrWhiteSpace( this.ApiKey ) ? PredefinedValues.NotAvailable : this.ApiKey )},ApiToken:{( string.IsNullOrWhiteSpace( this._sessionId ) ? PredefinedValues.NotAvailable : this._sessionId )},Store:{( string.IsNullOrWhiteSpace( this.Store ) ? PredefinedValues.NotAvailable : this.Store )}}}";
-		}
+			try
+			{
+				this.getSessionIdSemaphore.Wait();
+				if( !string.IsNullOrWhiteSpace( this._sessionId ) && DateTime.UtcNow.Subtract( this._sessionIdCreatedAt ).TotalSeconds < SessionIdLifeTime )
+					return new GetSessionIdResponse( this._sessionId, true );
 
-		private string CreateMethodCallInfo( string methodParameters = "", Mark mark = null, string errors = "", string methodResult = "", string additionalInfo = "", [ CallerMemberName ] string memberName = "", string notes = "" )
-		{
-			mark = mark ?? Mark.Blank();
-			var connectionInfo = this.ToJsonSoapInfo();
-			var str = string.Format(
-				"{{MethodName:{0}, ConnectionInfo:{1}, MethodParameters:{2}, Mark:\"{3}\"{4}{5}{6}{7}}}",
-				memberName,
-				connectionInfo,
-				methodParameters,
-				mark,
-				string.IsNullOrWhiteSpace( errors ) ? string.Empty : ", Errors:" + errors,
-				string.IsNullOrWhiteSpace( methodResult ) ? string.Empty : ", Result:" + methodResult,
-				string.IsNullOrWhiteSpace( notes ) ? string.Empty : ", Notes:" + notes,
-				string.IsNullOrWhiteSpace( additionalInfo ) ? string.Empty : ", AdditionalInfo: " + additionalInfo
-				);
-			return str;
+				var sessionId = await this.PullSessionId().ConfigureAwait( false );
+
+				this._sessionIdCreatedAt = sessionId.Item2;
+				this._sessionId = sessionId.Item1;
+				this.TokenSecret = this._sessionId;
+
+				return new GetSessionIdResponse( this._sessionId, false );
+			}
+			catch( Exception exc )
+			{
+				if( throwException )
+					throw new MagentoSoapException( string.Format( "An error occured during GetSessionId()" ), exc );
+				else
+				{
+					this.LogTraceGetResponseException( exc );
+					return null;
+				}
+			}
+			finally
+			{
+				this.getSessionIdSemaphore.Release();
+			}
 		}
+		#endregion
 
 		#region JustForTesting
 		public async Task< int > CreateCart( string storeid )
