1006b130c84984400f6c182beecf37660bb2dd3b
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-09-07 00:47:09 +0300

    m2.0.2: fixed m2.0.2 ping

--- src/MagentoAccess/Services/Soap/2_0_2_0_ce/MagentoServiceLowLevelSoap_v_2_0_2_0_ce_Misc.cs
+++ src/MagentoAccess/Services/Soap/2_0_2_0_ce/MagentoServiceLowLevelSoap_v_2_0_2_0_ce_Misc.cs
@@ -247,12 +247,12 @@ namespace MagentoAccess.Services.Soap._2_0_2_0_ce
 				// Magento doesn't provide method to receive magento vesrion, since Magento2.0 thats why we use backEndMoodules API
 
 				await this.GetSessionId().ConfigureAwait( false );
-				var modules = await this.GetBackEndModulesAsync().ConfigureAwait( false );
+				//var modules = await this.GetBackEndModulesAsync().ConfigureAwait( false );
 				var getOrdersResponse = await this.GetOrdersAsync( DateTime.Now, DateTime.Now.AddHours( 1 ) ).ConfigureAwait( false );
 				var getProductsRes = await this.GetProductsAsync( 1, null, false ).ConfigureAwait( false );
 
 				//var saveMethodResult = await this.SaveOrderMethodExistAsync().ConfigureAwait( false );
-				return modules?.Modules != null && modules.Modules.Count > 0 && getOrdersResponse.Orders.Count() >= 0 && getProductsRes.Products.Count() >= 0 ? new GetMagentoInfoResponse( "2.0.2.0", "CE" ) : null;
+				return /*modules?.Modules != null && modules.Modules.Count > 0 &&*/ getOrdersResponse.Orders.Count() >= 0 && getProductsRes.Products.Count() >= 0 ? new GetMagentoInfoResponse( "2.0.2.0", "CE" ) : null;
 			}
 			catch( Exception exc )
 			{
