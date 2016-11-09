801f28188098e73b8fa817544acc4f6a4c3e3af8
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-11-09 06:42:38 +0300

    private search criterias removed

--- src/MagentoAccess/Models/Services/Rest/v2x/Products/product.cs
+++ src/MagentoAccess/Models/Services/Rest/v2x/Products/product.cs
@@ -303,32 +303,6 @@ namespace MagentoAccess.Models.Services.Rest.v2x.Products
 		public List<CustomAttribute> customAttributes { get; set; }
 	}
 
-	public class Filter
-	{
-		public string field { get; set; }
-		public string value { get; set; }
-		public string conditionType { get; set; }
-	}
-
-	public class FilterGroup
-	{
-		public List<Filter> filters { get; set; }
-	}
-
-	public class SortOrder
-	{
-		public string field { get; set; }
-		public string direction { get; set; }
-	}
-
-	public class SearchCriteria
-	{
-		public List<FilterGroup> filterGroups { get; set; }
-		public List<SortOrder> sortOrders { get; set; }
-		public int pageSize { get; set; }
-		public int currentPage { get; set; }
-	}
-
 	public class RootObject
 	{
 		public List<Item> items { get; set; }
