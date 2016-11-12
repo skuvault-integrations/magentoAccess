6b30e5d87bf12ac0f5ca53464880eb9e701b91e2
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-11-12 16:37:45 +0300

    search criteria refactored

--- src/MagentoAccess/Models/Services/Rest/v2x/SearchCriteria.cs
+++ src/MagentoAccess/Models/Services/Rest/v2x/SearchCriteria.cs
@@ -1,45 +1,44 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace MagentoAccess.Models.Services.Rest.v2x
 {
 	public static class SearchCriteriaExtensions
 	{
-		public static string StringValue( this SearchCriteria.FilterGroup.Filter.ConditionType type )
+		public static string StringValue( this Filter.ConditionType type )
 		{
 			switch( type )
 			{
-				case SearchCriteria.FilterGroup.Filter.ConditionType.Equals:
+				case Filter.ConditionType.Equals:
 					return @"eq";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.ValueInSet:
+				case Filter.ConditionType.ValueInSet:
 					return @"finset";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.From:
+				case Filter.ConditionType.From:
 					return @"from";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.GreaterThan:
+				case Filter.ConditionType.GreaterThan:
 					return @"gt";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.GreaterThanOrEqual:
+				case Filter.ConditionType.GreaterThanOrEqual:
 					return @"gteq";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.In:
+				case Filter.ConditionType.In:
 					return @"in";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.Like:
+				case Filter.ConditionType.Like:
 					return @"like";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.LessThan:
+				case Filter.ConditionType.LessThan:
 					return @"lt";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.LessThanOrEqual:
+				case Filter.ConditionType.LessThanOrEqual:
 					return @"lteq";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.MoreOrEqual:
+				case Filter.ConditionType.MoreOrEqual:
 					return @"moreq";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.NotEqual:
+				case Filter.ConditionType.NotEqual:
 					return @"neq";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.NotIn:
+				case Filter.ConditionType.NotIn:
 					return @"nin";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.NotNull:
+				case Filter.ConditionType.NotNull:
 					return @"notnull";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.Null:
+				case Filter.ConditionType.Null:
 					return @"null";
-				case SearchCriteria.FilterGroup.Filter.ConditionType.To:
+				case Filter.ConditionType.To:
 					return @"to";
 			}
 			return null;
@@ -48,120 +47,78 @@ namespace MagentoAccess.Models.Services.Rest.v2x
 
 	public class SearchCriteria
 	{
-		public SearchCriteria()
-		{
-			this.FilterGroups = new List< FilterGroup >();
-		}
-
-		public SearchCriteria( IEnumerable< FilterGroup > filtergroups )
-			: this()
-		{
-			this.AddRange( filtergroups );
-		}
-
-		public int CurrentPage { get; set; }
-		public List< FilterGroup > FilterGroups { get; set; }
-		public int PageSize { get; set; }
-
-		public void Add( FilterGroup filtergroup )
-		{
-			this.FilterGroups.Add( filtergroup );
-		}
-
-		public void AddRange( IEnumerable< FilterGroup > filtergroups )
-		{
-			foreach( var group in filtergroups )
-			{
-				this.Add( group );
-			}
-		}
+		public List< FilterGroup > filter_groups { get; set; }
+		public List< SortOrder > sort_orders { get; set; }
+		public int page_size { get; set; }
+		public int current_page { get; set; }
 
 		public override string ToString()
 		{
-			var groups = this.FilterGroups.ToArray();
+			var groups = this.filter_groups.ToArray();
 			var sb = new StringBuilder();
 			for( var i = 0; i < groups.Length; i++ )
 			{
-				for( var j = 0; j < groups[ i ].Filters.Count(); j++ )
+				for( var j = 0; j < groups[ i ].filters.Count; j++ )
 				{
-					var filters = groups[ i ].Filters.ToArray();
+					var filters = groups[ i ].filters.ToArray();
 					if( sb.Length > 0 )
 						sb.Append( @"&" );
-					sb.Append( $@"searchCriteria[filter_groups][{i}][filters][{j}][field]={filters[ j ].Field}" + $@"&searchCriteria[filter_groups][{i}][filters][{j}][value]={Uri.EscapeDataString( filters[ j ].Value )}" );
-					if( filters[ j ].Condition.HasValue )
-						sb.Append( $@"&searchCriteria[filter_groups][{i}][filters][{j}][condition_type]={filters[ j ].Condition.Value.StringValue()}" );
+					sb.Append( $@"searchCriteria[filter_groups][{i}][filters][{j}][field]={filters[ j ].field}" + $@"&searchCriteria[filter_groups][{i}][filters][{j}][value]={Uri.EscapeDataString( filters[ j ].value )}" );
+					if( !string.IsNullOrWhiteSpace( filters[ j ].condition_type ) )
+						sb.Append( $@"&searchCriteria[filter_groups][{i}][filters][{j}][condition_type]={filters[ j ].condition_type}" );
 				}
 			}
 			if( sb.Length == 0 )
 				sb.Append( @"searchCriteria=" );
-			if( this.CurrentPage > 0 )
-				sb.Append( $@"&searchCriteria[current_page]={this.CurrentPage}" );
-			if( this.PageSize > 0 )
-				sb.Append( $@"&searchCriteria[page_size]={this.PageSize}" );
+			if( this.current_page > 0 )
+				sb.Append( $@"&searchCriteria[current_page]={this.current_page}" );
+			if( this.page_size > 0 )
+				sb.Append( $@"&searchCriteria[page_size]={this.page_size}" );
 			return sb.ToString();
 		}
+	}
 
-		public class FilterGroup
+	public class FilterGroup
+	{
+		public List< Filter > filters { get; set; }
+	}
+
+	public class Filter
+	{
+		public string field { get; set; }
+		public string value { get; set; }
+		public string condition_type { get; set; }
+
+		public Filter( string field, string value, ConditionType? condition = null )
 		{
-			public FilterGroup()
-			{
-				this.Filters = new List< Filter >();
-			}
-
-			public FilterGroup( IEnumerable< Filter > filters )
-				: this()
-			{
-				this.AddRange( filters );
-			}
-
-			public List< Filter > Filters { get; set; }
-
-			public void Add( Filter filter )
-			{
-				this.Filters.Add( filter );
-			}
-
-			public void AddRange( IEnumerable< Filter > filters )
-			{
-				foreach( var filter in filters )
-				{
-					this.Add( filter );
-				}
-			}
-
-			public class Filter
-			{
-				public Filter( string field, string value, ConditionType? condition = null )
-				{
-					this.Field = field;
-					this.Value = value;
-					this.Condition = condition;
-				}
-
-				public enum ConditionType
-				{
-					Equals,
-					ValueInSet,
-					From,
-					To,
-					GreaterThan,
-					GreaterThanOrEqual,
-					In,
-					Like,
-					LessThan,
-					LessThanOrEqual,
-					MoreOrEqual,
-					NotEqual,
-					NotIn,
-					NotNull,
-					Null
-				}
-
-				public ConditionType? Condition { get; private set; }
-
-				public string Field { get; private set; }
-				public string Value { get; private set; }
-			}
+			this.field = field;
+			this.value = value;
+			this.condition_type = condition != null ? condition.ToString() : null;
 		}
+
+		public enum ConditionType
+		{
+			Equals,
+			ValueInSet,
+			From,
+			To,
+			GreaterThan,
+			GreaterThanOrEqual,
+			In,
+			Like,
+			LessThan,
+			LessThanOrEqual,
+			MoreOrEqual,
+			NotEqual,
+			NotIn,
+			NotNull,
+			Null
+		}
+	}
+
+	public class SortOrder
+	{
+		public string field { get; set; }
+		public string direction { get; set; }
 	}
 }
