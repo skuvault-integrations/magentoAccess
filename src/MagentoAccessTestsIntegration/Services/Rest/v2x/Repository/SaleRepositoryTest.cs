f016c60fe8fe322f1824a5a12dac95188b170845
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-11-12 20:01:09 +0300

    m2:rest:tests refactored

--- src/MagentoAccessTestsIntegration/Services/Rest/v2x/Repository/SaleRepositoryTest.cs
+++ src/MagentoAccessTestsIntegration/Services/Rest/v2x/Repository/SaleRepositoryTest.cs
@@ -13,7 +13,7 @@ namespace MagentoAccessTestsIntegration.Services.Rest.v2x.Repository
 	{
 		[ Test ]
 		[ TestCaseSource( typeof( RepositoryTestCases ), "TestCases" ) ]
-		public async Task GetOrdersAsync_StoreContainsOrders_ReceiveOrders( RepositoryTestCase testCase )
+		public async Task GetOrdersAsync_StoreContainsOrders_ReceivPage( RepositoryTestCase testCase )
 		{
 			//------------ Arrange
 			var adminRepository = new IntegrationAdminTokenRepository( testCase.Url );
@@ -27,28 +27,31 @@ namespace MagentoAccessTestsIntegration.Services.Rest.v2x.Repository
 
 			tokenTask.Result.Token.Should().NotBeNullOrWhiteSpace();
 			products.Result.items.Count.Should().BeGreaterOrEqualTo( 0 );
+			products.Result.items.Count.Should().BeLessOrEqualTo( 5 );
 		}
+
+
 
 		[ Test ]
 		[ TestCaseSource( typeof( RepositoryTestCases ), "TestCases" ) ]
-		public async Task GetOrdersAsync_StoreContainsOrders_ReceiveOrdersById( RepositoryTestCase testCase )
+		public async Task GetOrdersAsync_StoreContainsOrders_ReceiveOrdersByIdReceivePage( RepositoryTestCase testCase )
 		{
 			//------------ Arrange
 			var adminRepository = new IntegrationAdminTokenRepository( testCase.Url );
 			var tokenTask = adminRepository.GetToken( testCase.MagentoLogin, testCase.MagentoPass );
 			tokenTask.Wait();
-			var productRepository = new SalesOrderRepositoryV1( tokenTask.Result, testCase.Url );
+			var salesOrderRepositoryV1 = new SalesOrderRepositoryV1( tokenTask.Result, testCase.Url );
 			//------------ Act
 
-			var products = productRepository.GetOrdersAsync( DateTime.MinValue, DateTime.MaxValue, new PagingModel( 5, 1 ) );
-			products.Wait();
+			var items = salesOrderRepositoryV1.GetOrdersAsync( DateTime.MinValue, DateTime.MaxValue, new PagingModel( 5, 1 ) );
+			items.Wait();
 
-			var products2 = productRepository.GetOrdersAsync( products.Result.items.Select( x => x.increment_id ), new PagingModel( 5, 1 ) );
-			products2.Wait();
+			var items2 = salesOrderRepositoryV1.GetOrdersAsync( items.Result.items.Select( x => x.increment_id ), new PagingModel( 5, 1 ) );
+			items2.Wait();
 
 			tokenTask.Result.Token.Should().NotBeNullOrWhiteSpace();
-			products2.Result.items.Count.Should().BeGreaterOrEqualTo( 0 );
-			products2.Result.items.Count.Should().Be( products.Result.items.Count );
+			items2.Result.items.Count.Should().BeGreaterOrEqualTo( 0 );
+			items2.Result.items.Count.Should().Be( items.Result.items.Count );
 		}
 	}
 }
