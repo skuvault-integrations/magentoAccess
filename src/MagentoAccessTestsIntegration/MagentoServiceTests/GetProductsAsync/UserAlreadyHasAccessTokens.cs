6567d02cd446c174b382e735d816c67659f865d7
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2016-11-21 02:45:06 +0300

    m2:soap:  getstockitems tests refactored

--- src/MagentoAccessTestsIntegration/MagentoServiceTests/GetProductsAsync/UserAlreadyHasAccessTokens.cs
+++ src/MagentoAccessTestsIntegration/MagentoServiceTests/GetProductsAsync/UserAlreadyHasAccessTokens.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using FluentAssertions;
 using MagentoAccessTestsIntegration.TestEnvironment;
 using NUnit.Framework;
@@ -17,10 +18,13 @@ namespace MagentoAccessTestsIntegration.MagentoServiceTests.GetProductsAsync
 			var magentoService = this.CreateMagentoService( credentials.SoapApiUser, credentials.SoapApiKey, "null", "null", "null", "null", credentials.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials.MagentoVersion, credentials.GetProductsThreadsLimit, credentials.SessionLifeTimeMs );
 
 			// ------------ Act
-			var getProductsTask = magentoService.GetProductsAsync( includeDetails : true );
+			var getProductsTask = magentoService.GetProductsAsync( includeDetails : false );
 			getProductsTask.Wait();
 
 			// ------------ Assert
+			var items = getProductsTask.Result.ToList();
+			var dupl = items.GroupBy( x => x.ProductId ).Count( x => x.Count() > 1 );
+			dupl.Should().Be( 0 );
 			getProductsTask.Result.Should().NotBeNullOrEmpty();
 		}
 	}
