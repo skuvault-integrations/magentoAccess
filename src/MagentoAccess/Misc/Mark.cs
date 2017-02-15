57a11d08c75cc78b1b98ea875b337911a4e4df94
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2017-02-15 15:11:01 +0300

    mark parent added

--- src/MagentoAccess/Misc/Mark.cs
+++ src/MagentoAccess/Misc/Mark.cs
@@ -5,6 +5,7 @@ namespace MagentoAccess.Misc
 	public class Mark
 	{
 		public string MarkValue { get; private set; }
+		public Mark Parrent { get; private set; }
 
 		public static Mark Blank()
 		{
@@ -13,12 +14,22 @@ namespace MagentoAccess.Misc
 
 		public static Mark CreateNew()
 		{
-			return new Mark( Guid.NewGuid().ToString() );
+			return CreateNew( null );
 		}
 
-		public Mark( string markValue )
+		public static Mark CreateNew( Mark parent )
 		{
-			MarkValue = markValue;
+			return new Mark( Guid.NewGuid().ToString(), parent );
+		}
+
+		public Mark( string markValue ) : this( markValue, null )
+		{
+		}
+
+		public Mark( string markValue, Mark parent )
+		{
+			this.MarkValue = markValue;
+			this.Parrent = parent;
 		}
 
 		public override string ToString()
@@ -28,7 +39,7 @@ namespace MagentoAccess.Misc
 
 		public override int GetHashCode()
 		{
-			return this.MarkValue.GetHashCode();
+			return this.MarkValue.GetHashCode() ^ this.Parrent.GetHashCode();
 		}
 
 		#region Equality members
@@ -38,7 +49,7 @@ namespace MagentoAccess.Misc
 				return false;
 			if( ReferenceEquals( this, other ) )
 				return true;
-			return string.Equals( this.MarkValue, other.MarkValue, StringComparison.InvariantCultureIgnoreCase );
+			return string.Equals( this.MarkValue, other.MarkValue, StringComparison.InvariantCultureIgnoreCase ) && this.Parrent.Equals( other.Parrent );
 		}
 
 		public override bool Equals( object obj )
@@ -59,6 +70,11 @@ namespace MagentoAccess.Misc
 		public static bool IsBlank( this Mark source )
 		{
 			return source == null || string.IsNullOrWhiteSpace( source.MarkValue );
+		}
+
+		public static Mark CreateChildOrNull( this Mark source )
+		{
+			return source != null ? Mark.CreateNew( source ) : null;
 		}
 
 		public static string ToStringSafe( this Mark source )
