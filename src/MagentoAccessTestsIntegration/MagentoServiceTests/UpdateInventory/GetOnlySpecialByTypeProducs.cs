b287503076a0f20cb6fbcd987b094345bf6098c5
MaximKitsenko <maxim.kitsenko.r31@gmail.com>
2017-03-18 19:25:11 +0300

    update test added

--- src/MagentoAccessTestsIntegration/MagentoServiceTests/UpdateInventory/GetOnlySpecialByTypeProducs.cs
+++ src/MagentoAccessTestsIntegration/MagentoServiceTests/UpdateInventory/GetOnlySpecialByTypeProducs.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using MagentoAccess;
 using MagentoAccess.Misc;
+using MagentoAccess.Models;
+using MagentoAccess.Models.PutInventory;
 using MagentoAccessTestsIntegration.TestEnvironment;
+using Netco.Extensions;
 using NUnit.Framework;
 
 namespace MagentoAccessTestsIntegration.MagentoServiceTests.GetProductsAsync
@@ -19,19 +24,28 @@ namespace MagentoAccessTestsIntegration.MagentoServiceTests.GetProductsAsync
 		{
 			// ------------ Arrange
 			var magentoService = this.CreateMagentoService( credentials.SoapApiUser, credentials.SoapApiKey, "null", "null", "null", "null", credentials.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials.MagentoVersion, credentials.GetProductsThreadsLimit, credentials.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
+			var rnd = new Random( DateTime.UtcNow.Millisecond );
+			var val = rnd.Next( 100, 1500 );
 
 			// ------------ Act
-			var getProductsTask1 = magentoService.GetProductsAsync( new[] { 0, 1 }, includeDetails : true, productType : "simple", excludeProductByType : false, mark : new Mark( nameof(InventoryUpdated) + "_s" ) );
+			var getProductsTask1 = magentoService.GetProductsAsync( new[] { 0, 1 }, includeDetails : true, productType : "simple", excludeProductByType : false, mark : new Mark( nameof( InventoryUpdated ) + "_s" ) );
 			Task.WhenAll( getProductsTask1 ).Wait();
 
-			magentoService.UpdateInventoryAsync
+			var src = getProductsTask1.Result.DeepClone().ToInventory( x => int.Parse( x.Qty ) );
+			var inventories = src as IList< Inventory > ?? src.ToList();
+			inventories.ForEach( x => x.Qty = val );
+			var getProductsTask2 = magentoService.UpdateInventoryAsync( inventories );
+			Task.WhenAll( getProductsTask2 ).Wait();
+
+			var getProductsTask3 = magentoService.GetProductsAsync( new[] { 0, 1 }, includeDetails : true, productType : "simple", excludeProductByType : false, mark : new Mark( nameof( InventoryUpdated ) + "_s" ) );
+			Task.WhenAll( getProductsTask3 ).Wait();
 
 			// ------------ Assert
 			getProductsTask1.Result.Should().NotBeNullOrEmpty();
-			getProductsTask2.Result.Should().NotBeNullOrEmpty();
+			getProductsTask3.Result.Should().NotBeNullOrEmpty();
 
-			getProductsTask1.Result.All( x => x.ProductType == "simple" ).Should().BeTrue();
-			getProductsTask2.Result.All( x => x.ProductType == "bundle" ).Should().BeTrue();
+			getProductsTask1.Result.Any( x => x.Qty != val.ToString() ).Should().BeTrue();
+			getProductsTask3.Result.All( x => x.Qty == val.ToString() ).Should().BeTrue();
 		}
 	}
 }
