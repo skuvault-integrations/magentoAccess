7b4006c5af52b9b204e6c029bcdd6a52442a89b6
Sergey Shumakov <sergey.shumakov@skuvault.com>
2017-06-10 12:05:04 +0300

    Fix loading products with codes "%00" (v. 1.7.0-1.9.0)

--- src/MagentoAccess/Services/Soap/1_7_0_1_ce_1_9_0_1_ce/MagentoServiceLowLevelSoap_v_from_1_7_to_1_9_CE_Products.cs
+++ src/MagentoAccess/Services/Soap/1_7_0_1_ce_1_9_0_1_ce/MagentoServiceLowLevelSoap_v_from_1_7_to_1_9_CE_Products.cs
@@ -21,9 +21,6 @@ namespace MagentoAccess.Services.Soap._1_7_0_1_ce_1_9_0_1_ce
 				{
 					var sourceList = Enumerable.Range( start1, count1 ).Select( selector1 ).ToList();
 
-					if( sourceList.RemoveAll( x => x == "%00" ) > 0 )
-						sourceList.Add( "%*00" );
-
 					var productsResponses = await sourceList.ProcessInBatchAsync( this._getProductsMaxThreads, async x => await this.GetProductsAsync( productType, productTypeShouldBeExcluded, x, updatedFrom ).ConfigureAwait( false ) ).ConfigureAwait( false );
 					var prods = productsResponses.SelectMany( x => x.Products ).ToList();
 					return prods;
