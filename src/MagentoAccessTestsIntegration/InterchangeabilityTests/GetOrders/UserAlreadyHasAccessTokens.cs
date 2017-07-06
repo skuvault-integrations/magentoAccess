dbbf3939d5d022b1800384a42e82d81b501d109a
Maxim Kitsenko <macros5@ya.ru>
2017-07-06 19:59:55 +0300

    intercheangability tests: test implemented

--- src/MagentoAccessTestsIntegration/InterchangeabilityTests/GetOrders/UserAlreadyHasAccessTokens.cs
+++ src/MagentoAccessTestsIntegration/InterchangeabilityTests/GetOrders/UserAlreadyHasAccessTokens.cs
@@ -19,6 +19,7 @@ namespace MagentoAccessTestsIntegration.InterchangeabilityTests.GetOrders
 		{
 			// ------------ Arrange
 			var magentoService = this.CreateMagentoService( credentials.SoapApiUser, credentials.SoapApiKey, "null", "null", "null", "null", credentials.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials.MagentoVersion, credentials.GetProductsThreadsLimit, credentials.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
+			var magentoService2 = this.CreateMagentoService( credentials2.SoapApiUser, credentials2.SoapApiKey, "null", "null", "null", "null", credentials2.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials2.MagentoVersion, credentials2.GetProductsThreadsLimit, credentials2.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
 
 			// ------------ Act
 			// var firstCreatedItem = this._orders[ credentials.StoreUrl ].OrderBy( x => x.UpdatedAt ).First();
@@ -28,16 +29,20 @@ namespace MagentoAccessTestsIntegration.InterchangeabilityTests.GetOrders
 			// var modifiedTo = new DateTime( ( lastCreatedItem.UpdatedAt ).Ticks, DateTimeKind.Utc ).AddSeconds( -1 );
 			var modifiedFrom = new DateTime( 2016, 1, 28, 23, 23, 59 ).AddSeconds( 1 );
 			var modifiedTo = new DateTime( 2017, 7, 2, 23, 30, 39 ).AddSeconds( -1 );
+
 			var getOrdersTask = magentoService.GetOrdersAsync( modifiedFrom, modifiedTo, new Mark( "TEST-GET-ORDERS" ) );
 			getOrdersTask.Wait();
+			var getOrdersTask2 = magentoService2.GetOrdersAsync(modifiedFrom, modifiedTo, new Mark("TEST-GET-ORDERS-2"));
+			getOrdersTask2.Wait();
 
 			// ------------ Assert
 			// var thatMustBeReturned = this._orders[ credentials.StoreUrl ].Where( x => x != firstCreatedItem && x != lastCreatedItem ).Select( x => x.OrderIncrementalId ).ToList();
 			// var thatWasReturned = getOrdersTask.Result.Where( x => x != firstCreatedItem && x != lastCreatedItem ).Select( x => x.OrderIncrementalId ).ToList();
-			var thatWasReturned = getOrdersTask.Result.Select( x => x.OrderIncrementalId ).ToList();
+			var thatWasReturned = getOrdersTask.Result.OrderBy( x => x.OrderIncrementalId ).ToList();
+			var thatWasReturned2 = getOrdersTask2.Result.OrderBy( x => x.OrderIncrementalId ).ToList();
 
 			// thatWasReturned.Should().BeEquivalentTo( thatMustBeReturned );
-			thatWasReturned.Should().NotBeNullOrEmpty();
+			thatWasReturned.Should().BeEquivalentTo(thatWasReturned2);
 		}
 	}
 }
