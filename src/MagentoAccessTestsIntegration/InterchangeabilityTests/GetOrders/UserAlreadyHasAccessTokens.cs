e92fa7c0a2b4fb2ffbc2b50902cb4034364d15dd
Maxim Kitsenko <macros5@ya.ru>
2017-07-07 01:55:41 +0300

    intercheangability tests: test refactored

--- src/MagentoAccessTestsIntegration/InterchangeabilityTests/GetOrders/UserAlreadyHasAccessTokens.cs
+++ src/MagentoAccessTestsIntegration/InterchangeabilityTests/GetOrders/UserAlreadyHasAccessTokens.cs
@@ -15,34 +15,26 @@ namespace MagentoAccessTestsIntegration.InterchangeabilityTests.GetOrders
 	{
 		[ Test ]
 		[ TestCaseSource( typeof( InterchangeabilityTestCases ), "TestStoresCredentials" ) ]
-		public void ReceiveOrders( MagentoServiceSoapCredentials credentials, MagentoServiceSoapCredentials credentials2 )
+		public void ReceiveOrders( MagentoServiceSoapCredentials credentialsRest, MagentoServiceSoapCredentials credentialsSoap )
 		{
 			// ------------ Arrange
-			var magentoService = this.CreateMagentoService( credentials.SoapApiUser, credentials.SoapApiKey, "null", "null", "null", "null", credentials.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials.MagentoVersion, credentials.GetProductsThreadsLimit, credentials.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
-			var magentoService2 = this.CreateMagentoService( credentials2.SoapApiUser, credentials2.SoapApiKey, "null", "null", "null", "null", credentials2.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentials2.MagentoVersion, credentials2.GetProductsThreadsLimit, credentials2.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
+			var magentoServiceRest = this.CreateMagentoService( credentialsRest.SoapApiUser, credentialsRest.SoapApiKey, "null", "null", "null", "null", credentialsRest.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentialsRest.MagentoVersion, credentialsRest.GetProductsThreadsLimit, credentialsRest.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
+			var magentoServiceSoap = this.CreateMagentoService( credentialsSoap.SoapApiUser, credentialsSoap.SoapApiKey, "null", "null", "null", "null", credentialsSoap.StoreUrl, "http://w.com", "http://w.com", "http://w.com", credentialsSoap.MagentoVersion, credentialsSoap.GetProductsThreadsLimit, credentialsSoap.SessionLifeTimeMs, false, ThrowExceptionIfFailed.AllItems );
 
-			// ------------ Act
-			// var firstCreatedItem = this._orders[ credentials.StoreUrl ].OrderBy( x => x.UpdatedAt ).First();
-			// var lastCreatedItem = this._orders[ credentials.StoreUrl ].OrderBy( x => x.UpdatedAt ).Last();
-
-			// var modifiedFrom = new DateTime( ( firstCreatedItem.UpdatedAt ).Ticks, DateTimeKind.Utc ).AddSeconds( 1 );
-			// var modifiedTo = new DateTime( ( lastCreatedItem.UpdatedAt ).Ticks, DateTimeKind.Utc ).AddSeconds( -1 );
+			//------------Act
 			var modifiedFrom = new DateTime( 2016, 1, 28, 23, 23, 59 ).AddSeconds( 1 );
 			var modifiedTo = new DateTime( 2017, 7, 2, 23, 30, 39 ).AddSeconds( -1 );
 
-			var getOrdersTask = magentoService.GetOrdersAsync( modifiedFrom, modifiedTo, new Mark( "TEST-GET-ORDERS" ) );
+			var getOrdersTask = magentoServiceRest.GetOrdersAsync( modifiedFrom, modifiedTo, new Mark( "TEST-GET-ORDERS" ) );
 			getOrdersTask.Wait();
-			var getOrdersTask2 = magentoService2.GetOrdersAsync(modifiedFrom, modifiedTo, new Mark("TEST-GET-ORDERS-2"));
+			var getOrdersTask2 = magentoServiceSoap.GetOrdersAsync( modifiedFrom, modifiedTo, new Mark( "TEST-GET-ORDERS-2" ) );
 			getOrdersTask2.Wait();
 
-			// ------------ Assert
-			// var thatMustBeReturned = this._orders[ credentials.StoreUrl ].Where( x => x != firstCreatedItem && x != lastCreatedItem ).Select( x => x.OrderIncrementalId ).ToList();
-			// var thatWasReturned = getOrdersTask.Result.Where( x => x != firstCreatedItem && x != lastCreatedItem ).Select( x => x.OrderIncrementalId ).ToList();
+			//------------Assert
 			var thatWasReturned = getOrdersTask.Result.OrderBy( x => x.OrderIncrementalId ).ToList();
 			var thatWasReturned2 = getOrdersTask2.Result.OrderBy( x => x.OrderIncrementalId ).ToList();
 
-			// thatWasReturned.Should().BeEquivalentTo( thatMustBeReturned );
-			thatWasReturned.Should().BeEquivalentTo(thatWasReturned2);
+			thatWasReturned.Should().BeEquivalentTo( thatWasReturned2 );
 		}
 	}
 }
