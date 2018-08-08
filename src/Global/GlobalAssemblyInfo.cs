8a7b60094f15d849444360411150e2b2dd51c2e5
Maxim Kitsenko <maxim.kitsenko.r31@gmail.com>
2018-08-08 16:34:36 +0300

    build 2.7.0

--- src/Global/GlobalAssemblyInfo.cs
+++ src/Global/GlobalAssemblyInfo.cs
@@ -25,7 +25,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-// Keep in track with CA API version
 // 2.4.8.0 - magento 2 supported, 8 build, 4 subversion (includes significant features and codechanges)
 
-[ assembly : AssemblyVersion( "2.6.5.0" ) ]
+[ assembly : AssemblyVersion( "2.7.0.0" ) ]
