7850dc6485538e952a8a73051d887478325a3104
Shumer <SShumer@users.noreply.github.com>
2021-07-30 18:33:51 +0300

    ROYAL-1834 Update ActionPolicy for REST requests to handle all exceptions (#33)

--- src/MagentoAccess/Services/Rest/v2x/Repository/ActionPolicies.cs
+++ src/MagentoAccess/Services/Rest/v2x/Repository/ActionPolicies.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using MagentoAccess.Misc;
 using Netco.ActionPolicyServices;
@@ -9,26 +7,7 @@ namespace MagentoAccess.Services.Rest.v2x.Repository
 {
 	internal static class ActionPolicies
 	{
-		public static ActionPolicyAsync RepeatOnChannelProblemAsync { get; } = ActionPolicyAsync.From( ( exception =>
-		{
-			var magentoWebException = exception as MagentoWebException;
-			if ( magentoWebException != null 
-				&& magentoWebException.StatusCode != null )
-			{
-				switch( magentoWebException.StatusCode )
-				{
-					case HttpStatusCode.NotFound:
-					case HttpStatusCode.BadRequest:
-					case HttpStatusCode.ServiceUnavailable:
-					case HttpStatusCode.BadGateway:
-						return true;
-					default:
-						return false;
-				}
-			}
-
-			return false;
-		} ) )
+		public static ActionPolicyAsync RepeatOnChannelProblemAsync { get; } = ActionPolicyAsync.Handle< Exception >()
 			.RetryAsync( 7, async ( ex, i ) =>
 			{
 				MagentoLogger.Log().Trace( ex, "Retrying Magento API call due to channel problem for the {0} time", i );
